0dfec86604ab3bdf06e4a0a9f546c49acf5e0c16
semchuk <easy.easy.ru@mail.ru>
2017-08-21 21:51:08 +1000

    Add first modal window

---
+++ scripts/modal.js
@@ -0,0 +1,8 @@
+function closeModal() {
+  var modal = document.getElementByClassname('wrapper1')[0];
+  modal.style.display = "none";
+}
+function openModal() {
+  var modal = document.getElementByClassname('wrapper1')[0];
+  modal.style.display = "block";
+}
