4a9f8bbb5172641d84836b82e2512384d3995a1b
semchuk <easy.easy.ru@mail.ru>
2017-08-21 22:21:33 +1000

    One moment

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,8 +1,16 @@
 function closeModal() {
-  var modal = document.getElementByClassname('wrapper1')[0];
+  var modal = document.getElementById('wrapper1');
   modal.style.display = "none";
 }
 function openModal() {
-  var modal = document.getElementByClassname('wrapper1')[0];
+  var modal = document.getElementById('wrapper1');
+  modal.style.display = "block";
+}
+function closeModal1() {
+  var modal = document.getElementById('wrapper2');
+  modal.style.display = "none";
+}
+function openModal1() {
+  var modal = document.getElementById('wrapper2');
   modal.style.display = "block";
 }
