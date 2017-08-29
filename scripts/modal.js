9e53dc6c4d7cd1e5723f2b99d1f3d4878ac48c6f
semchuk <easy.easy.ru@mail.ru>
2017-08-29 14:48:42 +1000

    Mobile version is ready

--- scripts/modal.js
+++ scripts/modal.js
@@ -16,13 +16,21 @@ function openModal1() {
 }
 function closeOpen() {
   var login = document.getElementById('wrapper1');
-  var reg = document.getElementById('wrapper2')
+  var reg = document.getElementById('wrapper2');
   reg.style.display = "block";
   login.style.display = "none";
 }
 function closeOpen1() {
-  var login = document.getElementById('wrapper1');
-  var reg = document.getElementById('wrapper2')
   reg.style.display = "none";
   login.style.display = "block";
+}
+window.onclick = function (event) {
+  var modal = document.getElementsByClassName("wrapper")[0];
+  var modal1 = document.getElementsByClassName("wrapper")[1];
+  if (event.target == modal){
+    modal.style.display = 'none';
+  }
+  if (event.target == modal1){
+        modal1.style.display = 'none';
+  }
 }
